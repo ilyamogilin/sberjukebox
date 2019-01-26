4b9b73be16e759f2cfd1a0609799eec18ee4d915
Alex <anoshkin.allex@gmail.com>
2019-01-27 01:02:29 +0300

    change path to ws

--- src/main/java/com/sber/jukeBox/config/WebSocketConfig.java
+++ src/main/java/com/sber/jukeBox/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
 }
