99838441b1d63fa273748222394a71542eb5bdb8
Alex <anoshkin.allex@gmail.com>
2019-01-26 20:51:37 +0300

    тестирую rest controller

--- src/main/java/com/sber/jukeBox/controller/MusicController.java
+++ src/main/java/com/sber/jukeBox/controller/MusicController.java
@@ -1,5 +1,14 @@
 package com.sber.jukeBox.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MusicController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "123";
+    }
 
 }
