908eda9cacd28f010f0c5b9018a25fe737c946fa
Alex <anoshkin.allex@gmail.com>
2019-01-27 06:49:44 +0300

    почистил инициализицию приложения

--- src/main/java/com/sber/jukeBox/Application.java
+++ src/main/java/com/sber/jukeBox/Application.java
@@ -1,31 +1,10 @@
 package com.sber.jukeBox;
 
-import com.sber.jukeBox.datastore.JukeBoxStoreImpl;
-import com.sber.jukeBox.model.TrackEntity;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-
-//    @Bean
-//    public CommandLineRunner demoData() {
-//        return args -> {
-//
-//            TrackEntity track1 = new TrackEntity(1, 1, "track name", "artist", "someUrl");
-//            TrackEntity track2 = new TrackEntity(1, 1, "track", "art", "url");
-//            TrackEntity track3 = new TrackEntity(2, 1, "track name2", "artist2", "someUrl2");
-//
-//            JukeBoxStoreImpl jukeBoxStore = JukeBoxStoreImpl.getInstance();
-//
-//            jukeBoxStore.addTrack(track1);
-//            jukeBoxStore.addTrack(track2);
-//            jukeBoxStore.addTrack(track3);
-//            jukeBoxStore.getTracksWithNowPlaying(1);
-//        };
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/sber/jukeBox/controller/MusicController.java
+++ src/main/java/com/sber/jukeBox/controller/MusicController.java
@@ -5,10 +5,8 @@ import com.sber.jukeBox.json.TrackList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
