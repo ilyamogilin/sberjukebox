c67033b63b9a6cc1c89f2e88a193fdaa12a604b5
Alex <anoshkin.allex@gmail.com>
2019-01-27 07:24:40 +0300

    добавил кнопку для оплаты счета

--- src/main/java/com/sber/jukeBox/controller/PaymentController.java
+++ src/main/java/com/sber/jukeBox/controller/PaymentController.java
@@ -4,7 +4,6 @@ import com.sber.jukeBox.datastore.InvoiceList;
 import com.sber.jukeBox.model.Invoice;
 import com.sber.jukeBox.vk.MessageSender;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,18 +15,16 @@ public class PaymentController {
     @Autowired
     private MessageSender sender;
 
-    @Bean
-    public InvoiceList invoiceList() {
-        return InvoiceList.getInstance();
-    }
-
     @Autowired
     private InvoiceList invoiceList;
 
     @RequestMapping("/invoice/{id}")
     public String getInvoice(@PathVariable(value="id") int id, Model model) {
 
+        System.out.println(invoiceList.getInvoice(id));
+
         model.addAttribute("invoice", invoiceList.getInvoice(id));
+        model.addAttribute("invoiceId", id);
 
         return "invoice";
     }
